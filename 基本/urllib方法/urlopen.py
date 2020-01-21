4763e8be4f81478244a1b700c88a69ead039d125
kurkol <52071654+kurkol@users.noreply.github.com>
2020-01-21 22:11:40 +0800

    Delete urlopen.py

--- 基本/urllib方法/urlopen.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-response = urllib.request.urlopen('https://www.baidu.com')
-print(type(response))
-print(response.status)
-print(response.getheaders())
-print(response.getheader('Server'))
-
-c=input()
