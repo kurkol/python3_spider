587e25b5223ab418995ae846428ebb3d3b9d371c
kurkol <52071654+kurkol@users.noreply.github.com>
2020-01-30 20:55:17 +0800

    Update SaveCookies_txt.py

--- 基本/ urllib方法/处理程序/SaveCookies_txt.py
+++ 基本/ urllib方法/处理程序/SaveCookies_txt.py
@@ -1,6 +1,6 @@
 import http.cookiejar, urllib.request
 
-filename = 'cookie.txt'
+filename = 'cookies.txt'
 cookie = http.cookiejar.MozillaCookieJar(filename)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 opener = urllib.request.build_opener(handler)
