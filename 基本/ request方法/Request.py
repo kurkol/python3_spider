14df77539bc33895b022b158ff203c18c82b7d48
kurkol <52071654+kurkol@users.noreply.github.com>
2020-02-08 21:24:39 +0800

    Create Request.py

---
+++ 基本/ request方法/Request.py
@@ -0,0 +1,13 @@
+from requests import Request, Session
+
+url = 'http://httpbin.org/post'
+data = {'name' : 'germey'}
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+s = Session()
+req = Request('POST', url, data=data, headers=headers)
+prepped = s.prepare_request(req)
+r = s.send(prepped)
+print(r.text)
+
+c=input()
