5f03aa743b84a1d59538bca14cae923b992c2bc7
kurkol <52071654+kurkol@users.noreply.github.com>
2020-02-13 17:51:44 +0800

    Create maoyanrank.py

---
+++ 基本/maoyanrank.py
@@ -0,0 +1,42 @@
+import re
+import requests
+import json
+import time
+from requests.exceptions import RequestException
+
+def get_one_page(url):
+    headers ={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(url, headers = headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index board-index.*?(.*?)</i>>*?class="board-img" src="(.*?)"name.*?a.*?>(.*?)</a>.*?star.*?(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>')
+    items = re.findall(pattern, html)
+    for item in items:
+        yield{
+            'index':item[0],
+            'image':item[1],
+            'title':item[2].strip(),
+            'actor':item[3].strip()[3:1] if len(item[3]) > 3 else '',
+            'time':item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score':tiem[5].strip() + item[6].strip
+        }
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        print(type(json.dumps(content)))
+        f.write(json.dumps(content, ensure_ascii=False)+'\n')
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+if __name__ =='__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
