4c09aca4c53022adcc4145f54214706eec3de5fa
kurkol <52071654+kurkol@users.noreply.github.com>
2020-03-14 20:58:38 +0800

    Create child.py

---
+++ 解析/BeautifulSoup/child.py
@@ -0,0 +1,36 @@
+html = '''
+<html>
+<head>
+<title>The Dormouse's story</title>
+</head>
+<body>
+<p class="story">
+    Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1">
+<span>Elsie</span>
+</a>
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a>
+and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.
+</p>
+<p class="story">...</p>
+'''
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html, 'lxml')
+
+
+print(soup.p.contents)
+#contents取直接子节点，结果是一个list
+
+print(soup.p.children)
+for i, child in enumerate(soup.p.children):
+    print(i, child)
+#children同样取直接子节点，所以span节点没有被单独列出
+
+print(soup.p.descendants)
+for i, child in enumerate(soup.p.descendants):
+    print(i, child)
+#descendants取子孙节点
