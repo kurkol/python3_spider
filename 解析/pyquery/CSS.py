3a5695954d78365a9643b4dfafe68d529a19c335
kurkol <52071654+kurkol@users.noreply.github.com>
2020-03-17 20:43:37 +0800

    Update CSS.py

--- 解析/pyquery/CSS.py
+++ 解析/pyquery/CSS.py
@@ -1,10 +1,10 @@
 html = '''
 <div id='container'>
 <ul class='list'>
-<li class="item-0"><a href="link1.html">first item</a></li>
+<li class="item-0">first item</a></li>
 <li class="item-1"><a href="link2.html">second item</a></li>
-<li class="item-inactive"><a href="link3.html">third item</a></li>
-<li class="item-1"><a href="link4.html">fourth item</a></li>
+<li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+<li class="item-1 active"><a href="link4.html">fourth item</a></li>
 <li class="item-0"><a href="link5.html">fifth item</a>
 </ul>
 </div>
