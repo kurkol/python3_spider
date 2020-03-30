5e41a4033513029e0f310b8dbd6ff9ba7ecae622
kurkol <52071654+kurkol@users.noreply.github.com>
2020-03-30 22:45:51 +0800

    Update insert.py

--- 存储/MySQL/insert.py
+++ 存储/MySQL/insert.py
@@ -19,3 +19,16 @@ except:
     db.rollback()
     #执行失败，数据回滚
 db.close()
+
+#作为一个事物应当具有ACID特性
+#atomicity,原子性，要么都做要么不做
+#consistency,一致性，从一个一致性状态到另一个一致性状态，如总量不变的样本
+#isolation,隔离性，事物之间不互相干扰
+#durability,持久性，事物一旦提交对数据库的改变时永久的
+
+#数据库标准操作可以写为：
+#try:
+#   cursor.execute(sql)
+#   db.commit()
+#except:
+#   db.rollback()
