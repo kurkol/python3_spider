e3571f2179502fbd9acad235845da475b453268f
kurkol <52071654+kurkol@users.noreply.github.com>
2020-04-20 12:28:41 +0800

    Update find.py

--- 存储/MongoDB/find.py
+++ 存储/MongoDB/find.py
@@ -1,7 +1,7 @@
 import pymongo
 
 client = pymongo.MongoClient(host='localhost', port=27017)
-//client = pymongo.MongoClient('mongodb://localhost:27017/')
+#client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.test
 collection = db.students
 
