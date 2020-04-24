89bbaee9b12768e16888e07bd3525059af3977bd
kurkol <52071654+kurkol@users.noreply.github.com>
2020-04-24 16:48:17 +0800

    Create execute_script.py

---
+++ 动态爬取/execute_script.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.zhihu.com/explore')
+browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+browser.execute_script('alert("To Bottom")')
+
+#window.scrollTo(xpos,ypos)
